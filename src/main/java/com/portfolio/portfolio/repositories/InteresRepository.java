d82172f703cb49e590cc0cdf4292e675a5f7dfe4
Marito048 <estudio1802@gmail.com>
2022-04-14 13:28:20 -0300

    subiendo

--- src/main/java/com/portfolio/portfolio/repositories/InteresRepository.java
+++ src/main/java/com/portfolio/portfolio/repositories/InteresRepository.java
@@ -1,14 +1,10 @@
 package com.portfolio.portfolio.repositories;
-
-import javax.persistence.Table;
-
 import com.portfolio.portfolio.models.InteresModel;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@Table(name = "interes")
+
 public interface InteresRepository extends CrudRepository<InteresModel, Integer> {
 
 }
