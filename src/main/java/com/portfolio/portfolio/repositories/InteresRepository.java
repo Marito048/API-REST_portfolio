578632efd9d371adff7b28b1722a69924ea8119d
Marito048 <estudio1802@gmail.com>
2022-04-29 16:59:56 -0300

    commit

--- src/main/java/com/portfolio/portfolio/repositories/InteresRepository.java
+++ src/main/java/com/portfolio/portfolio/repositories/InteresRepository.java
@@ -2,10 +2,11 @@ package com.portfolio.portfolio.repositories;
 
 import com.portfolio.portfolio.models.InteresModel;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InteresRepository extends JpaRepository <InteresModel , Long>{
+public interface InteresRepository extends CrudRepository <InteresModel , Long>{
     
 }
