c8a7a3692e97d8ab00756b2e0769f1104f2cb2ef
Marito048 <estudio1802@gmail.com>
2022-04-29 21:20:50 -0300

    commit

--- src/main/java/com/portfolio/portfolio/models/SkillHardModel.java
+++ src/main/java/com/portfolio/portfolio/models/SkillHardModel.java
@@ -18,10 +18,34 @@ public class SkillHardModel {
     @Column(unique = true, nullable = false)
      Long id_duras;
      
-    @Basic
-    Long porcentaje_duras;
-    String titulo_duras;
-    String descripcion_duras;
+    
+     Long porcentaje_duras;
+     String titulo_duras;
+     String descripcion_duras;
+
+     public Long getPorcentaje_duras() {
+         return this.porcentaje_duras;
+     }
+
+     public void setPorcentaje_duras(Long porcentaje_duras) {
+         this.porcentaje_duras = porcentaje_duras;
+     }
+
+     public String getTitulo_duras() {
+         return this.titulo_duras;
+     }
+
+     public void setTitulo_duras(String titulo_duras) {
+         this.titulo_duras = titulo_duras;
+     }
+
+     public String getDescripcion_duras() {
+         return this.descripcion_duras;
+     }
+
+     public void setDescripcion_duras(String descripcion_duras) {
+         this.descripcion_duras = descripcion_duras;
+     }
 
     
 }
--- src/main/java/com/portfolio/portfolio/models/SkillSoftModel.java
+++ src/main/java/com/portfolio/portfolio/models/SkillSoftModel.java
@@ -16,10 +16,33 @@ public class SkillSoftModel {
     @Column(unique = true, nullable = false)
      Long id_blandas;
     
-    @Basic
-    Long porcentaje_blandas;
-    String titulo_blandas;
-    String descripcion_blandas;
+     Long porcentaje_blandas;
+     String titulo_blandas;
+     String descripcion_blandas;
+
+     public Long getPorcentaje_blandas() {
+         return this.porcentaje_blandas;
+     }
+
+     public void setPorcentaje_blandas(Long porcentaje_blandas) {
+         this.porcentaje_blandas = porcentaje_blandas;
+     }
+
+     public String getTitulo_blandas() {
+         return this.titulo_blandas;
+     }
+
+     public void setTitulo_blandas(String titulo_blandas) {
+         this.titulo_blandas = titulo_blandas;
+     }
+
+     public String getDescripcion_blandas() {
+         return this.descripcion_blandas;
+     }
+
+     public void setDescripcion_blandas(String descripcion_blandas) {
+         this.descripcion_blandas = descripcion_blandas;
+     }
 
     
 }
