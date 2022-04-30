816638d079c1268b2b6d5026b2681ce92b3d8513
Marito048 <estudio1802@gmail.com>
2022-04-30 19:02:54 -0300

    commit

--- src/main/java/com/portfolio/portfolio/controllers/SkillHardController.java
+++ src/main/java/com/portfolio/portfolio/controllers/SkillHardController.java
@@ -1,36 +1,46 @@
 package com.portfolio.portfolio.controllers;
+
 import java.util.ArrayList;
 
 import com.portfolio.portfolio.models.SkillHardModel;
-import com.portfolio.portfolio.services.SkillHardService;
+import com.portfolio.portfolio.repositories.SkillHardRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hard")
 public class SkillHardController {
     @Autowired
-    SkillHardService skillHardService;
+    SkillHardRepository skillHardRepository;
 
     @GetMapping("/get")
     @ResponseBody
-    public ArrayList<SkillHardModel> obtenerSkill(){
-        return skillHardService.obtenerSkill();
+    public ArrayList<SkillHardModel> obtenerSkillHard(){
+        return (ArrayList<SkillHardModel>)skillHardRepository.findAll();
+
+    
     }
     @PostMapping("/new")
-    public void crearSkill (SkillHardModel skill){
-        skillHardService.crearSkill(skill);
+    public void crearSkillHard(@RequestBody SkillHardModel skillHard){
+            skillHardRepository.save(skillHard);
     }
     @PutMapping("/update")
-    public void modificarSkill (SkillHardModel skill){
-        skillHardService.guardarSkill(skill);
+    public void modificarSkillHard(@RequestBody SkillHardModel skillHard){
+            skillHardRepository.save(skillHard);
     }
-    @DeleteMapping("/delete/{id_duras}")
-    public void borrrarSkill (@PathVariable Long id_duras){
-        skillHardService.borrarSkill(id_duras);
+    @DeleteMapping("/delete/{id_SkillHard}")
+    public void borrarSkillHard(@PathVariable Long id_SkillHard){
+        skillHardRepository.deleteById(id_SkillHard);
     }
     
-     
 }
+
