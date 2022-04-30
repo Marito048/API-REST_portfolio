3a5e415a38dbb4b7cb426147f030c833917874b0
Marito048 <estudio1802@gmail.com>
2022-04-30 19:03:12 -0300

    commit

--- src/main/java/com/portfolio/portfolio/models/SkillHardModel.java
+++ src/main/java/com/portfolio/portfolio/models/SkillHardModel.java
@@ -1,52 +1,25 @@
 package com.portfolio.portfolio.models;
 
-import javax.persistence.*;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter @Setter 
 @Entity
-@Table(name="habilidadesDuras")
-
-
+@Table(name = "habilidadesDuras")
 public class SkillHardModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(unique = true, nullable = false)
-     Long id_duras;
-     
-    
-     @Basic
-     String porcentaje_duras;
-     String titulo_duras;
-     String descripcion_duras;
-
-     public String getPorcentaje_duras() {
-         return this.porcentaje_duras;
-     }
-
-     public void setPorcentaje_duras(String porcentaje_duras) {
-         this.porcentaje_duras = porcentaje_duras;
-     }
-
-     public String getTitulo_duras() {
-         return this.titulo_duras;
-     }
-
-     public void setTitulo_duras(String titulo_duras) {
-         this.titulo_duras = titulo_duras;
-     }
-
-     public String getDescripcion_duras() {
-         return this.descripcion_duras;
-     }
-
-     public void setDescripcion_duras(String descripcion_duras) {
-         this.descripcion_duras = descripcion_duras;
-     }
-
-    
+        Long id_duras;
+    @Basic
+        String porcentaje_duras;
+    @Basic
+        String titulo_duras;
+    @Basic
+        String descripcion_duras;
+        
 }
--- src/main/java/com/portfolio/portfolio/services/SkillHardService.java
+++ src/main/java/com/portfolio/portfolio/services/SkillHardService.java
@@ -1,28 +1,32 @@
 package com.portfolio.portfolio.services;
+
 import java.util.ArrayList;
+
 import com.portfolio.portfolio.models.SkillHardModel;
 import com.portfolio.portfolio.repositories.SkillHardRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-
 public class SkillHardService {
     @Autowired
     SkillHardRepository skillHardRepository;
-
-    public ArrayList<SkillHardModel> obtenerSkill(){
-        return (ArrayList<SkillHardModel>) skillHardRepository.findAll();
+    public ArrayList<SkillHardModel> obtenerSkillHard(){
+        return (ArrayList<SkillHardModel>)skillHardRepository.findAll();
     }
-    public void crearSkill (SkillHardModel skill){
-        skillHardRepository.save(skill);
+    public void crearSkillHard(SkillHardModel skillHard){
+        skillHardRepository.save(skillHard);
     }
-    public void guardarSkill (SkillHardModel skill){
-        skillHardRepository.save(skill);
+    public void  guardarSkillHard(SkillHardModel skillHard){
+        skillHardRepository.save(skillHard);
     }
-    public void borrarSkill (Long id_duras){
-        skillHardRepository.deleteById(id_duras);
+    public void  eliminarSkillHard(Long id_SkillHard){
+        skillHardRepository.deleteById(id_SkillHard);
     }
-
     
+
 }
+
+
+
