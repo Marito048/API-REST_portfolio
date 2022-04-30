d0816b04f07b616ff39901323d57c41cd9c7bb49
Marito048 <estudio1802@gmail.com>
2022-04-30 20:34:16 -0300

    commit

--- src/main/java/com/portfolio/portfolio/controllers/SkillHardController.java
+++ src/main/java/com/portfolio/portfolio/controllers/SkillHardController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import com.portfolio.portfolio.models.SkillHardModel;
 import com.portfolio.portfolio.repositories.SkillHardRepository;
+import com.portfolio.portfolio.services.SkillHardService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,26 +21,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hard")
 public class SkillHardController {
     @Autowired
-    SkillHardRepository skillHardRepository;
+    SkillHardService  skillHardService;
 
     @GetMapping("/get")
     @ResponseBody
     public ArrayList<SkillHardModel> obtenerSkillHard(){
-        return (ArrayList<SkillHardModel>)skillHardRepository.findAll();
+        return skillHardService.obtenerSkillHard();
 
     
     }
     @PostMapping("/new")
     public void crearSkillHard(@RequestBody SkillHardModel skillHard){
-            skillHardRepository.save(skillHard);
+            skillHardService.crearSkillHard(skillHard);
     }
     @PutMapping("/update")
     public void modificarSkillHard(@RequestBody SkillHardModel skillHard){
-            skillHardRepository.save(skillHard);
+            skillHardService.crearSkillHard(skillHard);
     }
     @DeleteMapping("/delete/{id_SkillHard}")
     public void borrarSkillHard(@PathVariable Long id_SkillHard){
-        skillHardRepository.deleteById(id_SkillHard);
+        skillHardService.eliminarSkillHard(id_SkillHard);
     }
     
 }
