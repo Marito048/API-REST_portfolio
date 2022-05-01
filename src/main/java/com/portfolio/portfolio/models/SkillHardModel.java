d62ec722122d2d02412e4d2cf1619333615f97ab
Marito048 <estudio1802@gmail.com>
2022-05-01 10:58:21 -0300

    commit

--- src/main/java/com/portfolio/portfolio/models/SkillHardModel.java
+++ src/main/java/com/portfolio/portfolio/models/SkillHardModel.java
@@ -8,6 +8,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 @Entity
 @Table(name = "habilidadesDuras")
 public class SkillHardModel {
