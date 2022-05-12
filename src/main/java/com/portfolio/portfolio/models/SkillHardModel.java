fcbc296819d18cba4b6bae9ec3b1e08f2bee559c
Marito048 <estudio1802@gmail.com>
2022-05-11 21:03:10 -0300

    sk

--- src/main/java/com/portfolio/portfolio/models/SkillHardModel.java
+++ src/main/java/com/portfolio/portfolio/models/SkillHardModel.java
@@ -6,50 +6,54 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter
 @Entity
 @Table(name = "habilidadesDuras")
 public class SkillHardModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    private int id_duras;
+     int id_duras;
 
-    private int porcentaje_duras;
-    private String titulo_duras;
-    private String descripcion_duras;
+     int porcentaje_duras;
+     String titulo_duras;
+     String descripcion_duras;
 
-	public int getId_duras() {
-		return this.id_duras;
-	}
 
-	public void setId_duras(int id_duras) {
-		this.id_duras = id_duras;
-	}
+	// public int getId_duras() {
+	// 	return this.id_duras;
+	// }
 
-	public int getPorcentaje_duras() {
-		return this.porcentaje_duras;
-	}
+	// public void setId_duras(int id_duras) {
+	// 	this.id_duras = id_duras;
+	// }
 
-	public void setPorcentaje_duras(int porcentaje_duras) {
-		this.porcentaje_duras = porcentaje_duras;
-	}
+	// public int getPorcentaje_duras() {
+	// 	return this.porcentaje_duras;
+	// }
 
-	public String getTitulo_duras() {
-		return this.titulo_duras;
-	}
+	// public void setPorcentaje_duras(int porcentaje_duras) {
+	// 	this.porcentaje_duras = porcentaje_duras;
+	// }
 
-	public void setTitulo_duras(String titulo_duras) {
-		this.titulo_duras = titulo_duras;
-	}
+	// public String getTitulo_duras() {
+	// 	return this.titulo_duras;
+	// }
 
-	public String getDescripcion_duras() {
-		return this.descripcion_duras;
-	}
+	// public void setTitulo_duras(String titulo_duras) {
+	// 	this.titulo_duras = titulo_duras;
+	// }
 
-	public void setDescripcion_duras(String descripcion_duras) {
-		this.descripcion_duras = descripcion_duras;
-	}
+	// public String getDescripcion_duras() {
+	// 	return this.descripcion_duras;
+	// }
+
+	// public void setDescripcion_duras(String descripcion_duras) {
+	// 	this.descripcion_duras = descripcion_duras;
+	// }
 
 
 
